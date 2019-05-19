c9d6df091025486f5c15e96efa92753ad5baf114
WetOnionBoi <37384973+WetOnionBoi@users.noreply.github.com>
2019-05-20 11:51:10 +1200

    Add files via upload

---
+++ School Canteen/School Canteen/untitled-1.py
@@ -0,0 +1,54 @@
+from bottle import run, route, view, get, post, request, static_file
+from itertools import count
+
+
+class Canteen:
+
+    #signifies a private variable. not to be used outside of this class.
+    _ids = count (0)
+
+    def __init__(self, food_name, food_image, food_stock, food_price ):
+        #not passing ID as we want it to create it.
+        self.id = next(self._ids)
+        self.name = food_name
+        self.image = food_image
+        self.stock = food_stock
+        self.price = food_price
+
+    #Test Data
+food =    [
+          Canteen("Sushi Roll pack", "sushdiggity.jpg", 5, "$10"),
+          Canteen("Hot dog and Chips", "hotdiggity.jpg",  12, "$8"),
+          Canteen("Ham and Cheese Sandwiches", "hamdiggity.jpg", 4, "$5")
+          ]
+
+@route("/")
+@view("index")
+def index():
+    #need this function to attach the decorators above.
+    pass
+
+@route("/menu")
+@view("menu")
+def Canteen():
+    data = dict (menu_list = food)
+    return data
+
+@route('/purchase-success/<item_id>', method = 'POST')
+@view('purchase-success')
+def purchase_success(item_id):
+    item_id = int(item_id)
+    found_item = None   
+    for item in food: 
+        if item.id == item_id:
+            found_item  = item
+    data = dict(item = found_item)
+    found_item.stock = found_item.stock - 1   #minus 1 from the amount of items in stock
+    return data 
+
+
+@route('/picture/<filename>')
+def serve_picture(filename):
+    return static_file(filename, root = './Images')
+
+run(host='0.0.0.0', port = 8080, reloader=True, debug=True)
